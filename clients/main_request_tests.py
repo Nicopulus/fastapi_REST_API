ed09c6b62ace9f80670142d67f5c7f91b7c192d5
Nico Cabezas <51125246+Nicopulus@users.noreply.github.com>
2025-03-29 11:47:12 +0100

    RequestTest class vollständig erstellt.

--- clients/main_request_tests.py
+++ clients/main_request_tests.py
@@ -44,8 +44,8 @@ class RequestTests:
     def __init__(self, base_url):
         self._base_url = base_url
     
-    def get_data(self, item_id):
-        get_url = self._base_url + "items/" +str(item_id)
+    def get_data(self, item_id, url_str="/items/"):
+        get_url = self._base_url + url_str +str(item_id)
         response = requests.get(get_url)
         if response.status_code == 200:
             return response.json()
@@ -53,16 +53,49 @@ class RequestTests:
             print(f"Request failed with code: {response.status_code}")
             return None
     
-    def post_data(self, data=None):
+    def post_data(self, url_str="/items/", data=None):
         if data is None:
             raise ValueError("Data cannot be None.")
-        response = requests.post(self._base_url, json=data)
+        response = requests.post(self._base_url + url_str, json=data)
         if response.status_code == 200:
             return response.json()
         else:
             print(f"Request failed with code: {response.status_code}")
             return None
+        
+    def post_2d_plot(self, url_str="plot_snp/", plotdata=None):
+        response = requests.post(self._base_url + url_str, json=plotdata)
+        if response.status_code == 200:
+            dir_path = os.path.dirname(__file__)
+            file_path = os.path.join(dir_path, "post_plot_received.png")
+            if os.path.exists(file_path):
+                os.remove(file_path)
 
+            with open(file_path, "wb") as file:
+                file.write(response.content)
+            file.close()
+            info_out = "Plot information received successfully with code: "
+        else:
+            info_out = "Request failed with code: "
+        return f'{info_out}{response.status_code}'
+
+    def get_2d_plot(self, url="plot_snp/"):
+        get_url = self._base_url + url	
+        response = requests.get(get_url)
+        if response.status_code == 200:
+            dir_path = os.path.dirname(__file__)
+            file_path = os.path.join(dir_path, "get_plot_received.png")
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
+            with open(file_path, "wb") as file:
+                file.write(response.content)
+            file.close()
+            info_out = "Plot received successfully with code: "
+        else:
+            info_out = f"Request failed with code: "
+        return f'{info_out}{response.status_code}'
+    
 if __name__ == "__main__":
     ###POST/GET_TESTS####
     # base_url = "http://127.0.0.1:8000/"
@@ -76,8 +109,16 @@ if __name__ == "__main__":
     # post_2d_plot(general_url, plotdata=plotdata)
     # get_2d_plot(general_url)
 
-####################################################
+
+#######POST/GET TEST CLASSES#############################################
     tester = RequestTests("http://localhost:8000/")
-    print(tester.get_data(10))
+    # print(tester.get_data(10))
+
     # data = {'name' : 'nico', 'price':100}
-    print(tester.post_data(data={'name' : 'nico', 'price': 100}))
+    # print(tester.post_data(data=data))
+    
+    # # plotdata = {'x_data' : [0, 1, 2, 3, 4, 5], 'y_data' : [0, 1, 2, 3, 4, 5], 'args': {'color': 'blue', 'label': 'Test'}}
+    plotdata = {'x_data' : [0, 1, 2, 3, 4, 5], 'y_data' : [0, 1, 2, 3, 4, 5]}
+    print(tester.post_2d_plot(plotdata=plotdata))
+
+    print(tester.get_2d_plot())
--- main_server.py
+++ main_server.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 import matplotlib as plt #-> TODO install it
 import io
 import matplotlib.pyplot as plt
+import os
 
 class Item(BaseModel):
     name: str
@@ -15,6 +16,11 @@ class Item(BaseModel):
 class PlotData(BaseModel):
     x_data: list
     y_data: list
+
+class PlotDiagram(BaseModel):
+    x_data: dict
+    y_data: dict
+    args: any
 
 app = FastAPI()
 
@@ -30,22 +36,28 @@ async def create_item(item: Item):
 
 @app.post("/plot_snp/")
 async def create_plot(plot: PlotData):
+    # plot = PlotData(x_data=[0, 1, 2, 3, 4, 5], y_data=[0, 1, 2, 3, 4, 5])
     plt.plot(plot.x_data, plot.y_data)
     plt.xlabel("X-label")
     plt.ylabel("Y-label")
-    img_path = r"2Dplot\plot.png"
+    dir_path = os.path.dirname(__file__)
+    subdir_name = os.path.join(dir_path, r"2Dplot")
+    
+    if not os.path.exists(subdir_name):
+        os.makedirs(subdir_name)
+    img_path = os.path.join(subdir_name, r"plot.png")
+    
+    if os.path.exists(img_path):
+        os.remove(img_path)
     plt.savefig(img_path, format = 'png')
     return responses.FileResponse(img_path, media_type="image/png")
-    # img = io.BytesIO()
-    # plt.savefig(img, format = 'png')
-    # img.seek(0)
-    # return img
 
 @app.get("/plot_snp/")
 async def get_created_plot():
-    img_path = r"2Dplot\plot.png"
+    dir_path = os.path.dirname(__file__)
+    img_path = os.path.join(dir_path, r"2Dplot\\plot.png")
     return responses.FileResponse(img_path, media_type="image/png")
 
-#to start the app in development mode: fastapi dev .main.py / also fastapi dev
-#to start the app in production mode: uvicorn main:app --reload / also fastapi run
+#to start the app in development mode: fastapi dev .main_server.py / also fastapi dev
+#to start the app in production mode: uvicorn main_server:app --reload / also fastapi run
 #D:\repos\fast_api_devs\fastapi_REST_API\.venv\Scripts> .\activate
